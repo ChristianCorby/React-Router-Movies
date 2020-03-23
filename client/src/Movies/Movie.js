04bdf5e02fd348c910d2b6be6ea299fbca160d68
Christian <christiancorby@gmail.com>
2020-03-23 18:56:42 -0400

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+
   useEffect(() => {
-    const id = 1;
+    const id = props.match.params.id;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -18,18 +17,16 @@ const Movie = (props) => {
           console.error(error);
         });
 
-  },[]);
-  
+  },[props.match.params.id]);
+
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
   //   addToSavedList(movie)
   // }
-
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
-
   const { title, director, metascore, stars } = movie;
   return (
     <div className="save-wrapper">
@@ -42,7 +39,6 @@ const Movie = (props) => {
           Metascore: <strong>{metascore}</strong>
         </div>
         <h3>Actors</h3>
-
         {stars.map(star => (
           <div key={star} className="movie-star">
             {star}
@@ -53,5 +49,4 @@ const Movie = (props) => {
     </div>
   );
 }
-
 export default Movie;
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const MovieList = props => {
@@ -21,12 +22,13 @@ const MovieList = props => {
   return (
     <div className="movie-list">
       {movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
-
 function MovieDetails({ movie }) {
   const { title, director, metascore, stars } = movie;
   return (
@@ -39,7 +41,6 @@ function MovieDetails({ movie }) {
         Metascore: <strong>{metascore}</strong>
       </div>
       <h3>Actors</h3>
-
       {stars.map(star => (
         <div key={star} className="movie-star">
           {star}
@@ -48,5 +49,4 @@ function MovieDetails({ movie }) {
     </div>
   );
 }
-
 export default MovieList;
